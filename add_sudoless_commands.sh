496a9bec2a6a1d810c3eb167531811490b25e44d
Albert Jan Grasmeijer <info@evenhosten.nl>
2024-01-23 14:49:26 +0100

    Modification to sudoless_commands

--- add_sudoless_commands.sh
+++ add_sudoless_commands.sh
@@ -1,40 +1,5 @@
 # #!/bin/bash
 
-# INPUT_FILE="sudoless_commands.txt"
-
-# if [ ! -f "$INPUT_FILE" ]; then
-#     echo "Input file not found: $INPUT_FILE"
-#     exit 1
-# fi
-
-# USERNAME=$(whoami)
-
-# SUDOERS_LINE="$USERNAME ALL=NOPASSWD:"
-
-# FILE_TO_CHANGE="/etc/sudoers"
-
-# SUDOERS_UNCHANGED=$(sudo cat "$FILE_TO_CHANGE")
-
-# INPUT_STRING=$(<"$INPUT_FILE" tr '\n' ',' | sed 's/,$//')
-
-# TO_ADD="$SUDOERS_LINE $INPUT_STRING"
-
-# TEMP_FILE=$(mktemp)
-
-# sudo sed -e "/$SUDOERS_LINE/d" "$FILE_TO_CHANGE" > "$TEMP_FILE"
-
-# echo -e "\n$TO_ADD" >> "$TEMP_FILE"
-
-# sudo chown root:root "$TEMP_FILE"
-
-# cat "$TEMP_FILE"
-
-# sudo mv "$TEMP_FILE" "$FILE_TO_CHANGE"
-
-# rm "$TEMP_FILE"
-
-#!/bin/bash
-
 SUDOERS_PATH="/etc/sudoers.d"
 
 INPUT_FILE="sudoless_commands.txt"
