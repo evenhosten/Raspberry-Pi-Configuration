15acc64c8b18c21fba7f20c84f9cc2287532b227
Albert Jan Grasmeijer <info@evenhosten.nl>
2024-02-09 14:42:46 +0100

    Some commit, too long ago to remember what exactly.
    But it looks like code improvement to me

--- add_sudoless_commands.sh
+++ add_sudoless_commands.sh
@@ -1,29 +1,49 @@
-# #!/bin/bash
+#!/bin/bash
 
-SUDOERS_PATH="/etc/sudoers.d"
-
-INPUT_FILE="sudoless_commands.txt"
-
-if [ ! -f "$INPUT_FILE" ]; then
-    echo "Input file not found: $INPUT_FILE"
-    exit 1
+if [ ! -f "~/.bash_custom" ]
+then
+    touch ~/.bash_custom
 fi
 
-USERNAME=$(whoami)
-
-if [ ! -f "$SUDOERS_PATH/$USERNAME.conf" ]; then
-    echo "Creating sudoers file for $USERNAME"
-    sudo touch "$SUDOERS_PATH/$USERNAME.conf"
-    sudo chown root:root "$SUDOERS_PATH/$USERNAME.conf"
-    sudo chmod 440 "$SUDOERS_PATH/$USERNAME.conf"
+# If source ~/.bash_custom is not in ~/.bashrc, add it
+if ! grep -q "source ~/.bash_custom" ~/.bashrc
+then
+    echo "source ~/.bash_custom" >> ~/.bashrc
 fi
 
-INPUT_STRING=$(<"$INPUT_FILE" tr '\n' ',' | sed 's/,$//')
+# If parameter --add/-a is passed, add the alias to the file
+if [ "$1" == "--add" ] || [ "$1" == "-a" ]
+then
 
-SUDOERS_LINE="$USERNAME ALL=NOPASSWD: $INPUT_STRING"
+    # If the alias already exists, exit
+    if grep -q "alias $2='sudo $2'" ~/.bash_custom
+    then
+        echo "Alias $2 already exists in ~/.bash_custom"
+    fi
 
-echo "$SUDOERS_LINE" | sudo tee -a "$SUDOERS_PATH/$USERNAME.conf" > /dev/null
+    echo "alias $2='sudo $2'" >> ~/.bash_custom
+    echo "Added alias $2 to ~/.bash_custom"
+fi
 
-echo "Added $INPUT_STRING to $SUDOERS_PATH/$USERNAME.conf"
+# If parameter --remove/-r is passed, remove the alias from the file
+if [ "$1" == "--remove" ] || [ "$1" == "-r" ]
+then
+    sed -i "/alias $2='sudo $2'/d" ~/.bash_custom
+    echo "Removed alias $2 from ~/.bash_custom"
+fi
+
+# If ~/.bash_custom is empty, remove it
+if [ ! -s "~/.bash_custom" ]
+then
+    rm ~/.bash_custom
+
+    # If source ~/.bash_custom is in ~/.bashrc, remove it
+    if grep -q "source ~/.bash_custom" ~/.bashrc
+    then
+        sed -i "/source ~/.bash_custom/d" ~/.bashrc
+    fi
+fi
+
+source ~/.bashrc
 
 exit 0
